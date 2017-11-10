434cd4c7c536778a8875a00887b2ccfbd67b52ba
Jeff <jmtrotz@gmail.com>
2017-11-10 18:10:00 -0500

    Implement JFileChooser
    Implement JFileChooser for viewing and opening saved notes instead of the hacky half ass crap I tried yesterday

--- src/notetaker/ViewNotes.java
+++ src/notetaker/ViewNotes.java
@@ -1,13 +1,18 @@
 package notetaker;
 
 // Import utilities
+import java.awt.Desktop;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.SwingConstants;
 
 /**
@@ -58,105 +63,84 @@ public class ViewNotes extends JFrame
         this.add(class5);
     }
     
-    /**
-     * Listener for the class0 button. Launches a new window for viewing
-     * saved notes
-     */
     public class Class0Listener implements ActionListener
     {
         @Override
         public void actionPerformed(ActionEvent e)
         {
-            Class0Notes class0Notes = new Class0Notes();
-            class0Notes.setTitle("CS 305");
-            class0Notes.setSize(700, 700);
-            class0Notes.setLocationRelativeTo(null);
-            class0Notes.setVisible(true);
+            String filePath = "C:\\Users\\jmtro\\Documents\\SE321\\demo\\CS305";
+            chooseFile(filePath);
         }
     }
-    
-    /**
-     * Listener for the class1 button. Launches a new window for viewing
-     * saved notes
-     */
+
     public class Class1Listener implements ActionListener
     {
         @Override
         public void actionPerformed(ActionEvent e)
         {
-            Class1Notes class1Notes = new Class1Notes();
-            class1Notes.setTitle("CS 405");
-            class1Notes.setSize(700, 700);
-            class1Notes.setLocationRelativeTo(null);
-            class1Notes.setVisible(true);
+            String filePath = "C:\\Users\\jmtro\\Documents\\SE321\\demo\\CS405";
+            chooseFile(filePath);
         }
     }
-    
-    /**
-     * Listener for the class2 button. Launches a new window for viewing
-     * saved notes
-     */
+
     public class Class2Listener implements ActionListener
     {
         @Override
         public void actionPerformed(ActionEvent e)
         {
-            Class2Notes class2Notes = new Class2Notes();
-            class2Notes.setTitle("CT 206");
-            class2Notes.setSize(700, 700);
-            class2Notes.setLocationRelativeTo(null);
-            class2Notes.setVisible(true);
+            String filePath = "C:\\Users\\jmtro\\Documents\\SE321\\demo\\CT206";
+            chooseFile(filePath);
         }
     }
-    
-    /**
-     * Listener for the class3 button. Launches a new window for viewing
-     * saved notes
-     */
+
     public class Class3Listener implements ActionListener
     {
         @Override
         public void actionPerformed(ActionEvent e)
         {
-            Class3Notes class3Notes = new Class3Notes();
-            class3Notes.setTitle("CT 406");
-            class3Notes.setSize(700, 700);
-            class3Notes.setLocationRelativeTo(null);
-            class3Notes.setVisible(true);
+            String filePath = "C:\\Users\\jmtro\\Documents\\SE321\\demo\\CT406";
+            chooseFile(filePath);
         }
     }
-    
-    /**
-     * Listener for the class4 button. Launches a new window for viewing
-     * saved notes
-     */
+
     public class Class4Listener implements ActionListener
     {
         @Override
         public void actionPerformed(ActionEvent e)
         {
-            Class4Notes class4Notes = new Class4Notes();
-            class4Notes.setTitle("SE 321");
-            class4Notes.setSize(700, 700);
-            class4Notes.setLocationRelativeTo(null);
-            class4Notes.setVisible(true);
+            String filePath = "C:\\Users\\jmtro\\Documents\\SE321\\demo\\SE321";
+            chooseFile(filePath);
         }
     }
     
-    /**
-     * Listener for the class4 button. Launches a new window for viewing
-     * saved notes
-     */
     public class Class5Listener implements ActionListener
     {
         @Override
         public void actionPerformed(ActionEvent e)
         {
-            Class5Notes class5Notes = new Class5Notes();
-            class5Notes.setTitle("SE 451");
-            class5Notes.setSize(700, 700);
-            class5Notes.setLocationRelativeTo(null);
-            class5Notes.setVisible(true);
+            String filePath = "C:\\Users\\jmtro\\Documents\\SE321\\demo\\SE451";
+            chooseFile(filePath);
+        }
+    }
+    
+    private void chooseFile(String filePath)
+    {
+        JFileChooser fileChooser = new JFileChooser(new File(filePath));
+        int selection = fileChooser.showOpenDialog(null);
+
+        if (selection == JFileChooser.APPROVE_OPTION) 
+        {
+            File fileToOpen = fileChooser.getSelectedFile();
+            
+            try 
+            {
+                Desktop.getDesktop().open(fileToOpen);
+            } 
+                
+            catch (IOException ex) 
+            {
+                Logger.getLogger(ViewNotes.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 }
