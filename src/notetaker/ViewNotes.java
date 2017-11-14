073d22c659461dea726bc6024af744a068095d20
kireh <kirehw@gmail.com>
2017-11-14 08:48:06 -0500

    Scripting intro!
    This update includes the beginning of a scripts folder to hold any ps1 files as well as the first connection point in java for a ps1 script to provide helper functions for the directory system

--- src/notetaker/ViewNotes.java
+++ src/notetaker/ViewNotes.java
@@ -5,13 +5,16 @@ import java.awt.Desktop;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.SwingConstants;
@@ -26,7 +29,7 @@ public class ViewNotes extends JFrame
     // Declare and set properties for attributes used in this class
 	private HashSet<String> classes = new HashSet<>();
     private final ImageIcon folderIcon = new ImageIcon("images/folder.jpg");
-    private String path = "C:\\notes"; 
+    private String path = System.getProperty("user.dir") + "\\notes"; 
    
     
     /**
@@ -34,6 +37,8 @@ public class ViewNotes extends JFrame
      */
     public ViewNotes() 
     {
+    	System.out.println(path);
+    	System.out.println(getClasses(path));
     	classes.add("CS 305");
     	classes.add("CS 405");
     	classes.add("CT 206");
@@ -52,6 +57,10 @@ public class ViewNotes extends JFrame
     	}
     }
     
+    /**
+     * Returns a row calculation for proper display in the grid
+     * @return number of rows as an int
+     */
     public int getRows()
     {
     	int items = classes.size();
@@ -64,6 +73,7 @@ public class ViewNotes extends JFrame
     		return items/2+1;
     	}
     }
+    
     /**
      * Attempts to set a new path for the ViewNotes window, changing the directory for the NoteTaker program.
      * @param newPath the new path to be set as a String
@@ -80,6 +90,10 @@ public class ViewNotes extends JFrame
     		{
     			success = true;
     			this.path = newPath;
+    		}
+    		else if (!testPath.exists())
+    		{
+    			
     		}
     	}
     	catch(NullPointerException npe)
@@ -110,76 +124,6 @@ public class ViewNotes extends JFrame
             chooseFile(filePath);
         }
     }
-
-/*    
-*     * Listener class for the class1 button
-*     
-*    public class Class1Listener implements ActionListener
-*    {
-*        @Override
-*        public void actionPerformed(ActionEvent e)
-*        {
-*            // If this button is selected, call chooseFile() and feed it the file path
-*            String filePath = "C:\\Users\\jmtro\\Documents\\SE321\\demo\\CS405";
-*            chooseFile(filePath);
-*        }
-*    }
-*
-*    
-*     * Listener class for the class2 button
-*    
-*    public class Class2Listener implements ActionListener
-*    {
-*        @Override
-*        public void actionPerformed(ActionEvent e)
-*        {
-*            // If this button is selected, call chooseFile() and feed it the file path
-*            String filePath = "C:\\Users\\jmtro\\Documents\\SE321\\demo\\CT206";
-*            chooseFile(filePath);
-*        }
-*    }
-*
-*    
-*     * Listener class for the class3 button
-*     
-*    public class Class3Listener implements ActionListener
-*    {
-*        @Override
-*        public void actionPerformed(ActionEvent e)
-*        {
-*            // If this button is selected, call chooseFile() and feed it the file path
-*            String filePath = "C:\\Users\\jmtro\\Documents\\SE321\\demo\\CT406";
-*            chooseFile(filePath);
-*        }
-*    }
-*
-*    
-*     * Listener class for the class4 button
-*     
-*    public class Class4Listener implements ActionListener
-*    {
-*        @Override
-*        public void actionPerformed(ActionEvent e)
-*        {
-*            // If this button is selected, call chooseFile() and feed it the file path
-*            String filePath = "C:\\Users\\jmtro\\Documents\\SE321\\demo\\SE321";
-*            chooseFile(filePath);
-*        }
-*    }
-*    
-*    
-*     * Listener class for the class5 button
-*     
-*    public class Class5Listener implements ActionListener
-*    {
-*        @Override
-*        public void actionPerformed(ActionEvent e)
-*        {
-*            // If this button is selected, call chooseFile() and feed it the file path
-*            String filePath = "C:\\Users\\jmtro\\Documents\\SE321\\demo\\SE451";
-*            chooseFile(filePath);
-*        }
-/*    }
     
     /**
      * Method to open the file explorer for viewing saved notes
@@ -213,18 +157,77 @@ public class ViewNotes extends JFrame
     }
     
     /**
+     * Adds a class to the list of viewable folders
+     * @param newClassName the name of the new class to be added
+     * @return success as a boolean
+     */
+    public boolean addClass(String newClassName)
+    {
+    	if (classes.add(newClassName))
+    	{
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+    }
+    
+    /**
+     * Will allow for a script to run in order to get and return an array of classes
+     * @param path the path to be searched as a String
+     * @return classes list as a String
+     */
+    public String getClasses(String path)
+    {
+    	String command = "powershell.exe  ls " +path;
+    	String out ="";
+    	
+    	try
+    	{
+    	  // Executing the command
+    	  Process powerShellProcess = Runtime.getRuntime().exec(command);
+    	  // Getting the results
+    	  powerShellProcess.getOutputStream().close();
+    	  BufferedReader stdout = new BufferedReader(new InputStreamReader(
+    			    powerShellProcess.getInputStream()));
+    	  while ((out = stdout.readLine()) != null) 
+    	  {
+    		   System.out.println(out);
+    	  }
+    	  return "yay";
+    	  
+    	}
+    	catch (SecurityException se)
+    	{
+    		return("getClasses isn't allowed to run powershell");
+    	}
+    	catch(IOException ioe)
+    	{
+    		return("getClasses has caught an IOException");
+    	}
+    	catch(NullPointerException npe)
+    	{
+    		return("null pointer in getClasses method");
+    	}
+    	catch(IllegalArgumentException iae)
+    	{
+    		return "illegal argument in getClasses";
+    	}
+    }
+    
+    /**
      * Encompasses the settings panel including all options available to the ViewNotes panel
      * @author kireh
      *
      */
-    class SettingsFrame extends JFrame
+    class SettingsFrame extends JDialog
     {
     	String path = "C:\\";
     	
     	public SettingsFrame(ViewNotes parent)
     	{
-    		super("Settings");
-    		this.path = parent.path;
+    		super(parent, "Settings", true);
     		
     	}
     }
